97f4ee44c62b61774f0066a40f474159837f90c6
Diego Fernando Calderon Silva <elingelindo@iMac-de-Diego.local>
2024-03-16 08:50:04 -0500

    Se genero un script para conceder permisos si el usuario es staff, es decir, solo quien tenga le permiso staff podra insertar o modificar registros de areas, de lo contrario solo podra ver el registro de areas

--- backEnd/cordisoft/backcoordisoft/areas/api/views.py
+++ backEnd/cordisoft/backcoordisoft/areas/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from areas.models import Area
 from areas.api.serializers import AreaSerializer
+from areas.api.permissions import IsAdminReadOnly
 
 class AreaApiViewSet(ModelViewSet):
+    permission_classes = [IsAdminReadOnly]
     serializer_class = AreaSerializer
     queryset = Area.objects.all()
