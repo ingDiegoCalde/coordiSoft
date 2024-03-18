d2b24d6b09c7f5e16816a60b5bca700da66238ce
Diego Fernando Calderon Silva <elingelindo@iMac-de-Diego.local>
2024-03-17 21:42:55 -0500

    Se genera un archivo de dependencias

--- backEnd/cordisoft/backcoordisoft/areas/api/views.py
+++ backEnd/cordisoft/backcoordisoft/areas/api/views.py
@@ -7,4 +7,5 @@ class AreaApiViewSet(ModelViewSet):
     permission_classes = [IsAdminReadOnly]
     serializer_class = AreaSerializer
     queryset = Area.objects.all()
+    #queryset = Area.objects.filter(state=True)
     lookup_field = 'slug' #esto cambia el endpoint diciendo que ya no busque o elimine por ID si no que ahora sera por slug.
