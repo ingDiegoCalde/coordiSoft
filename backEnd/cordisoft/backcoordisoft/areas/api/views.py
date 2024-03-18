6173a664a9546af1ec776cad0b13af8e0db8b3f8
diegokld30 <dfercalderon@poligran.edu.co>
2024-03-18 07:58:16 -0500

    Se genera un filtro para que busque por state o por name_area, esto con el fin de entender como se hace desde el back

--- backEnd/cordisoft/backcoordisoft/areas/api/views.py
+++ backEnd/cordisoft/backcoordisoft/areas/api/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
 from areas.models import Area
 from areas.api.serializers import AreaSerializer
 from areas.api.permissions import IsAdminReadOnly
@@ -7,5 +9,8 @@ class AreaApiViewSet(ModelViewSet):
     permission_classes = [IsAdminReadOnly]
     serializer_class = AreaSerializer
     queryset = Area.objects.all()
+    #Vamos a filtrar y a devolver lo que tenga estado en True
     #queryset = Area.objects.filter(state=True)
     lookup_field = 'slug' #esto cambia el endpoint diciendo que ya no busque o elimine por ID si no que ahora sera por slug.
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['state', 'name_area']
